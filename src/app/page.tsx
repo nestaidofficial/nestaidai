53a84e7bde86d50c33427c407fedaa9814290d84
Rahul Chettri <rahul@nestaid.com>
2026-03-30 12:21:56 -0400

    Remove LogoBar section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
-import { LogoBar } from "@/components/sections/LogoBar";
 import { Features } from "@/components/sections/Features";
 import { HowItWorks } from "@/components/sections/HowItWorks";
 import { Testimonials } from "@/components/sections/Testimonials";
@@ -12,7 +11,6 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <LogoBar />
         <Features />
         <HowItWorks />
         <Testimonials />
